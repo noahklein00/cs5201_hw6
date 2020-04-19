ab8c7b7b6c29c5b389b3c60b5933f8dc491c6143
Noah <nekgr8@mst.edu>
2020-04-19 18:53:22 -0500

    removed worthless files

--- basegate.cpp
+++
@@ -1,6 +0,0 @@
-// Programmer : Noah Klein
-// Instructor : Price
-// Class      : CS5201 Spring 2020
-// Assignment : Homework 6 - Complex Numbers, Outer Products, and Quantum
-//              Computing
-// Filename   : basegate.cpp
